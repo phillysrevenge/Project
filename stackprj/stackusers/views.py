2427c4e15565d4d064f98455b160250026f88e52
phillysrevenge <philemon.real@gmail.com>
2023-03-01 14:36:44 +0000

    project basic work

---
+++ stackprj/stackusers/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import userRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm, UserUpdateForm
+# Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = userRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Account created successfully for {username}')
+            return redirect('login')
+    else:
+        form = userRegisterForm()
+    return render(request, 'stackusers/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'stackusers/profile.html')
+
+
+def profile_update(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Successfully update profile!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'stackusers/profile_update.html', context)
