75f511807cf222dc06d91e86f2a31e359f72d59f
phillysrevenge <philemon.real@gmail.com>
2023-03-02 15:16:53 +0000

    questions page updated

--- stackprj/stackbase/urls.py
+++ stackprj/stackbase/urls.py
@@ -6,4 +6,8 @@ app_name = 'stackbase'
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
+
+    # CRUD QUESTIONs
+
+    path('questions/', views.QuestionListView.as_view(), name="question-list")
 ]
--- stackprj/stackbase/views.py
+++ stackprj/stackbase/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Question
 # Create your views here.
 
 
@@ -9,3 +10,11 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+# Crud
+
+
+class QuestionListView(ListView):
+    model = Question
+    context_object_name = 'questions'
+    ordering = ['-date_created']
